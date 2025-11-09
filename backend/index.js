db77fc884b1e12ccf67d513900005dd836c19f1a
Satendra-Soraiya <montymontysoriaiya@gmail.com>
2025-11-09 11:24:42 +0530

    chore(cors): allow hytrade-frontend.vercel.app and hytrade-dashboard.vercel.app

--- backend/index.js
+++ backend/index.js
@@ -93,6 +93,8 @@ const corsOptions = {
     'http://127.0.0.1:5173',
     'http://127.0.0.1:5174',
     'http://127.0.0.1:49604',
+    'https://hytrade-dashboard.vercel.app',
+    'https://hytrade-frontend.vercel.app',
     // Production URLs - Vercel
     /^https:\/\/.*\.vercel\.app$/,
     /^https:\/\/.*\.vercel\.com$/,
